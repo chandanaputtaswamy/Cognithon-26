59c1ecc096c7b2f43679812215a82fb030b2ee37
chandanaputtaswamy <chandanaputtaswamy481@gmail.com>
2026-02-20 20:41:18 +0530

    feat: remove github, linkedin and twitter icons from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
 import '../styles/Footer.css';
 
 const Footer = () => {
@@ -22,9 +22,6 @@ const Footer = () => {
                 <div className="footer-section">
                     <h4>Connect</h4>
                     <div className="social-icons">
-                        <a href="#" aria-label="Github"><FaGithub /></a>
-                        <a href="#" aria-label="LinkedIn"><FaLinkedin /></a>
-                        <a href="#" aria-label="Twitter"><FaTwitter /></a>
                         <a href="#" aria-label="Instagram"><FaInstagram /></a>
                     </div>
                 </div>
